51b4c5eeedd57167658bc8882fde157ecea94d43
Stephen <stephenmelnick8@gmail.com>
2019-06-24 17:44:56 +0000

    image blur 1

---
+++ imageblur.rb
@@ -0,0 +1,22 @@
+class Image
+  def initialize
+@image = ([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+  
+end
+
+
+def output_image
+  @image.each do |blur|
+  puts blur.join
+
+end
+end
+end
+
+image = Image.new
+image.output_image
